b7bdfb47c08bf7daff367fc5735632058a1df5ea
Mohammed reda Amali <mamali@e2r10p2.1337.ma>
2020-11-19 19:08:04 +0100

    ghdjuh

--- srcs/mysql.sh
+++ srcs/mysql.sh
@@ -1,10 +1,10 @@
 # get mysql
+export DEBIAN_FRONTEND="noninteractive"
+
+apt-get update && apt-get upgrade
+echo "mysql-apt-config mysql-apt-config/select-server select mysql-5.7" | debconf-set-selections
 wget http://repo.mysql.com/mysql-apt-config_0.8.13-1_all.deb
 dpkg -i mysql-apt-config_0.8.13-1_all.deb
 apt update
 apt install mysql-server -y
-#security changes on Database server
-systemctl restart mysql
-mysql_secure_installation
-# connect to the mysql database
-mysql -u root -p
+rm mysql-apt-config_0.8.14-1_all.deb
